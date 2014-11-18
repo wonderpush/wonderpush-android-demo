02e997b834eab0981f3f7a3870882b979b71d27a
Olivier Favre <olivier@yakaz.com>
2014-11-18 21:00:31 +0100

    `example` method for callback
    and a catchall receiver, to show a toast on method call,
    to help better visualize the SDK actions.

---
+++ src/com/wonderpush/demo/DemoApplication.java
@@ -0,0 +1,53 @@
+package com.wonderpush.demo;
+
+import com.wonderpush.sdk.WonderPush;
+
+import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.PatternMatcher;
+import android.support.v4.content.LocalBroadcastManager;
+import android.widget.Toast;
+
+public class DemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Example notification button action `method` receiver
+        IntentFilter exampleMethodIntentFilter = new IntentFilter();
+        exampleMethodIntentFilter.addAction(WonderPush.INTENT_NOTIFICATION_BUTTON_ACTION_METHOD_ACTION);
+        exampleMethodIntentFilter.addDataScheme(WonderPush.INTENT_NOTIFICATION_BUTTON_ACTION_METHOD_SCHEME);
+        exampleMethodIntentFilter.addDataAuthority(WonderPush.INTENT_NOTIFICATION_BUTTON_ACTION_METHOD_AUTHORITY, null);
+        exampleMethodIntentFilter.addDataPath("/example", PatternMatcher.PATTERN_LITERAL); // Note: prepend a / to the actual method name
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String arg = intent.getStringExtra(WonderPush.INTENT_NOTIFICATION_BUTTON_ACTION_METHOD_EXTRA_ARG);
+                Toast.makeText(context, "Example method successfully called with arg: " + arg,  Toast.LENGTH_LONG).show();
+            }
+        }, exampleMethodIntentFilter);
+
+        // Catch-all notification button action `method` receiver, to show the user that a method was called
+        IntentFilter catchallMethodIntentFilter = new IntentFilter();
+        catchallMethodIntentFilter.addAction(WonderPush.INTENT_NOTIFICATION_BUTTON_ACTION_METHOD_ACTION);
+        catchallMethodIntentFilter.addDataScheme(WonderPush.INTENT_NOTIFICATION_BUTTON_ACTION_METHOD_SCHEME);
+        catchallMethodIntentFilter.addDataAuthority(WonderPush.INTENT_NOTIFICATION_BUTTON_ACTION_METHOD_AUTHORITY, null);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String method = intent.getStringExtra(WonderPush.INTENT_NOTIFICATION_BUTTON_ACTION_METHOD_EXTRA_METHOD);
+                if ("example".equals(method)) {
+                    // The above receiver already took care of the "example" method
+                    return;
+                }
+                String arg = intent.getStringExtra(WonderPush.INTENT_NOTIFICATION_BUTTON_ACTION_METHOD_EXTRA_ARG);
+                Toast.makeText(context, "Method " + method + " called with arg: " + arg,  Toast.LENGTH_LONG).show();
+            }
+        }, catchallMethodIntentFilter);
+    }
+
+}
